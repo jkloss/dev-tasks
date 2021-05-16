fcecae2c61ed595c677124ea20c1434573dd1cd1
j.klos <notimportantemail@email.com>
2021-05-16 23:49:30 +0200

    Graphs fix

--- src/main/java/pl/jkloss/tasks/three/GraphsDetector.java
+++ src/main/java/pl/jkloss/tasks/three/GraphsDetector.java
@@ -1,28 +1,52 @@
 package pl.jkloss.tasks.three;
 
 import one.util.streamex.EntryStream;
+import one.util.streamex.StreamEx;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 class GraphsDetector {
 
+    private static final long MINIMAL_GRAPH_NUMBER = 1L;
+
     private final List<Entry<Integer, Integer>> pairs;
+    private final Set<Integer> connectedVertices;
 
     public GraphsDetector(List<Entry<Integer, Integer>> pairs) {
         this.pairs = pairs;
+        this.connectedVertices = getConnectedVertices(pairs);
     }
 
     public long getNumberOfDetectedGraphs() {
-        return EntryStream.ofPairs(pairs)
-                .removeKeyValue(this::anyConnected)
-                .count();
+        long numberOfPairsAfterGraphsSearch =
+                StreamEx.of(pairs)
+                        .remove(entry ->
+                                CollectionUtils
+                                        .containsAny(connectedVertices, entry.getKey(), entry.getValue()))
+                        .count();
+        if (((long) pairs.size()) == numberOfPairsAfterGraphsSearch) {
+            return numberOfPairsAfterGraphsSearch;
+        }
+        return MINIMAL_GRAPH_NUMBER + numberOfPairsAfterGraphsSearch;
     }
 
-    private boolean anyConnected(Entry<Integer, Integer> first,
-                                 Entry<Integer, Integer> second) {
+    private Set<Integer> getConnectedVertices(List<Entry<Integer, Integer>> sourcePairs) {
+        return EntryStream.ofPairs(sourcePairs)
+                .flatMapKeyValue(this::flatPossiblePairs)
+                .filter(entry ->
+                        entry.getKey().equals(entry.getValue()))
+                .map(Entry::getKey)
+                .toSet();
+    }
+
+    private StreamEx<Entry<Integer, Integer>> flatPossiblePairs(Entry<Integer, Integer> first,
+                                                                Entry<Integer, Integer> second) {
         return EntryStream.ofPairs(getEntriesContentAsList(first, second))
-                .anyMatch(Integer::equals);
+                .mapKeyValue(Map::entry);
     }
 
     private List<Integer> getEntriesContentAsList(Entry<Integer, Integer> first,
