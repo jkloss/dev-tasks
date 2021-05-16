34921465886a9039c9eef405d914f898bcb4d8f6
j.klos <notimportantemail@email.com>
2021-05-16 13:22:11 +0200

    Refactor

--- src/main/java/pl/jkloss/tasks/one/Main.java
+++ src/main/java/pl/jkloss/tasks/one/Main.java
@@ -13,16 +13,19 @@ final class Main {
     public static void main(String[] args) {
         System.out.println("Insert one-line, space separated, integer values (e.g. 1 2 3 4)");
         List<Integer> values = Commons.loadInput2IntegerList();
-        CustomStatistics customStatistics =
-                StreamEx.of(values)
-                        .distinct()
-                        .sorted()
-                        .toListAndThen(CustomStatistics::new);
+        CustomStatistics customStatistics = getCustomStatistics(values);
         displayIntegerList(customStatistics.getResultList());
         System.out.println("count: " + values.size());
         System.out.println("distinct: " + customStatistics.getCount());
         System.out.println("min: " + customStatistics.getMin());
         System.out.println("max: " + customStatistics.getMax());
+    }
+
+    private static CustomStatistics getCustomStatistics(List<Integer> values) {
+        return StreamEx.of(values)
+                .distinct()
+                .sorted()
+                .toListAndThen(CustomStatistics::new);
     }
 
     private static void displayIntegerList(List<Integer> list) {
